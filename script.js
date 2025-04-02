35be6d7c24299018f0c3ec74cea9dc4b68dbe513
Legend Paul <qamunyamaina950@gmail.com>
2025-04-02 20:43:20 +0300

    Adde Class to load Page To add Task

--- script.js
+++ script.js
@@ -4,31 +4,66 @@ let listTitle = document.querySelector(".list-title");
 let addTodoListBtn = document.querySelector(".add");
 let cancelDialog = document.querySelector(".cancel");
 let listCont =document.querySelector(".list-cont");
+let newListBtnText = document.querySelector(".new-list-btn p");
+let deleteList = document.querySelector(".bi-trash3");
+let editList = document.querySelector(".bi-pencil");
+let listName = document.querySelector(".list-name");
+let listAmount = document.querySelector(".amount");
 
-let names = [];
-function name(){
-    return `Paul`;
+function openDialog() {
+    newListDialog.showModal();
+    newListBtnText.innerHTML = "New List";
+    listTitle.value = "";
 }
-names.push(name());
 
-
-
-class TodoList {
-    listArray = [];
+class List {
+    listArray = [`
+            <h1>Todo List</h1>
+            <div class="list default" data-list-id=${0}>
+                <div class="name">
+                    <div class="icon">
+                        <i class="bi bi-list"></i>
+                    </div>
+                    <h3 class="list-name">Default</h3>
+                </div>
+                <p class="amount"></p>
+            </div>
+            <div class="list today" data-list-id=${1}>
+                <div class="name">
+                    <div class="icon">
+                        <i class="bi bi-calendar-day"></i>
+                    </div>
+                    <h3 class="list-name">Today</h3>
+                </div>
+                <p class="amount"></p>
+            </div>
+            <div class="list important" data-list-id=${2}>
+                <div class="name">
+                    <div class="icon">
+                        <i class="bi bi-star-fill"></i>
+                    </div>
+                    <h3 class="list-name">Important</h3>
+                </div>                
+                <p class="amount"></p>
+            </div>
+            <hr>
+        `];
     listKey = "list key";
     newTodoListName = '';
 
-    displayListOnLoad() {
-        document.addEventListener("DOMContentLoaded", ()=>{
-            this.getStorageList();
+    displayListOnLoad() {           
+            document.addEventListener("DOMContentLoaded", ()=>{
+                let storedList = localStorage.getItem(this.listKey);
+                if (!storedList){
+                    localStorage.setItem(this.listKey, JSON.stringify(this.listArray));
+                }
+                this.getStorageList();
+            
         });
     }
 
     openTodoDialog(){
-        newListBtn.addEventListener ("click", ()=>{
-            listTitle.value = ""; 
-            newListDialog.showModal();
-        });
+        newListBtn.addEventListener ("click", openDialog);
     }
     
     closeTodoDialog(){
@@ -39,11 +74,13 @@ class TodoList {
 
     createNewList(){
        return  `
-                    <div class="name">
-                        <div class="icon"></div>
-                        <h3>${this.newTodoListName}</h3>
-                    </div>                
-                    <p class="amount"></p>
+            <div class="name">
+                <div class="icon">
+                    <i class="bi bi-person-lines-fill"></i>
+                </div>
+                <h3 class="list-name">${this.newTodoListName}</h3>
+            </div>                
+            <p class="amount"></p>
             `;
         
     }
@@ -56,19 +93,27 @@ class TodoList {
 
     getStorageList() {
         let storedList = localStorage.getItem(this.listKey);
-        this.listArray = storedList ? JSON.parse(storedList) : []; // Parse or initialize as an empty array
-        for (let list of this.listArray) {
-            let newList = document.createElement("div");
-            newList.classList.add("list");
-            newList.innerHTML += list; 
-            listCont.appendChild(newList);
-        }
+        this.listArray = storedList ? JSON.parse(storedList) : [];
+        let listDataId = 3;
+        this.listArray.forEach((list, i) =>  {
+            if (i === 0) {        
+            listCont.innerHTML = list; // Clear the list container for the first item
+            }
+            else {
+                let newList = document.createElement("div");
+                newList.classList.add("list");
+                newList.setAttribute("data-list-id", listDataId);
+                listDataId++;
+                newList.innerHTML = list; 
+                listCont.appendChild(newList);
+            }
+            
+        });
     }
     addNewList(){
         let listTitleValue = listTitle.value;        
             if (listTitleValue){
                 this.newTodoListName = listTitleValue;
-    
                 this.addToStorage();
                 this.getStorageList();
     
@@ -94,6 +139,79 @@ class TodoList {
     }
 }
 
+class Task {
+    getClickedList() {
+        listCont.addEventListener("click", (e) => {
+            const clickedList = e.target.closest(".list"); 
+            if (clickedList) {
+                let listId = clickedList.dataset.listId; 
+                let listName = clickedList.querySelector(".list-name").textContent; 
+                if (listId > 2){
+                    listCont.innerHTML = this.openCreatedTaskPage(listName);
+                }                
+                else {
+                    listCont.innerHTML = this.openDefaultTaskPage(listName);
+                }
+                this.changeAddBtn();
+            }
+        });
+    }
+    changeAddBtn(){
+        newListBtnText.innerHTML = "New Task";
+        newListBtn.removeEventListener("click", openDialog);
+    }
+    openDefaultTaskPage(listName) {
+        return `
+            <div class="task-header">
+                <div class="left-content">
+                    <i class="bi bi-arrow-left"></i>
+                    <h2>${listName}</h2>
+                </div>
+            </div>
+        `;
+    }
+    openCreatedTaskPage(listName) {
+        return `
+            <div class="task-header">
+                <div class="left-content">
+                    <i class="bi bi-arrow-left"></i>
+                    <h1>${listName}</h1>
+                </div>
+                <div class="right-content">
+                    <i class="bi bi-trash3"></i>
+                    <i class="bi bi-pencil"></i>
+                </div>
+            </div>
+        `;
+    }
 
-let todoList = new TodoList();
-todoList.showList();
+    showTask() {
+        this.getClickedList(); 
+    }
+}
+
+class TodoList{
+    constructor() {
+        this.list = new List();
+        this.task = new Task();
+    }
+    moveToDefaultSection () {
+        document.addEventListener("click", (e)=>{
+            const clickedList = e.target.closest(".bi-arrow-left");
+            console.log(clickedList);
+            if (clickedList) {
+                this.list.getStorageList();
+                this.list.openTodoDialog();
+            }
+        });
+        
+    }
+    showList() {
+        this.list.showList();
+        this.task.showTask();
+        this.moveToDefaultSection();
+    }
+}
+
+const todoList = new TodoList();
+// todoList.showList();
